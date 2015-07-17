2574144221f8f076877738fcf58c8baa1ab4dd38
Iestyn Evans <Iestyn.Evans@bjss.com>
2015-07-17 15:12:24 +0100

    Replaced TLS_ENABLED property with sslConfig property

--- src/main/java/uk/nhs/ciao/fhir/RunCIP.java
+++ src/main/java/uk/nhs/ciao/fhir/RunCIP.java
@@ -1,12 +1,5 @@
 package uk.nhs.ciao.fhir;
 
-import java.util.Properties;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.support.StaticApplicationContext;
-import org.springframework.core.env.PropertiesPropertySource;
-
 import uk.nhs.ciao.camel.CamelApplication;
 import uk.nhs.ciao.camel.CamelApplicationRunner;
 import uk.nhs.ciao.exceptions.CIAOConfigurationException;
@@ -37,8 +30,6 @@ import static uk.nhs.ciao.util.GlobalConstants.*;
  *
  */
 public class RunCIP extends CamelApplication {
-	private static final Logger logger = LoggerFactory.getLogger(RunCIP.class);
-	
 	/**
 	 * This is the main class for running CIAO as a simple java process (e.g. within docker)
 	 * @param args Command line arguments (e.g. ETCD URL)
@@ -51,28 +42,5 @@ public class RunCIP extends CamelApplication {
 	
 	public RunCIP(final String[] args) throws CIAOConfigurationException {
 		super(CONFIG_FILE, args);
-	}
-	
-	@Override
-	protected StaticApplicationContext createParentApplicationContext()
-			throws CIAOConfigurationException {
-		final StaticApplicationContext context = super.createParentApplicationContext();
-		
-		final String sslConfig;
-		if (Boolean.parseBoolean(getCIAOConfig().getConfigValue("TLS_ENABLED"))) {
-			logger.info("TLS enabled");
-			sslConfig = "tls";
-		} else {
-			logger.info("TLS NOT enabled");
-			sslConfig = "vanilla";
-		}
-		
-		final Properties additionalProperties = new Properties();
-		additionalProperties.setProperty("sslConfig", sslConfig);
-		
-		context.getEnvironment().getPropertySources().addFirst(new PropertiesPropertySource(
-				"additionalProperties", additionalProperties));
-		
-		return context;
 	}
 }
